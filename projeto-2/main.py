50b2fc249f90b4ab28738a610768498345cd7e0e
Clevison <cleversonmaiden034@gmail.com>
2026-05-16 10:53:06 -0500

    finish
    depois averiguar erros

--- projeto-2/main.py
+++ projeto-2/main.py
@@ -3,6 +3,8 @@ import menu
 from cadastro import cadastrar_drones, listar
 from missoes import decolar, pousar, patrulhar
 from monitorar import monitorar
+from camera import ligar_camera, desligar_camera, tirar_foto
+from historico import ver_historico
 
 while True:
     menu.menu()
@@ -38,8 +40,26 @@ while True:
             
         elif opcao == 5:
             print("  Câmera  \n")
+            
+            print('''
+                  [1] - Desligar câmera
+                  [2] - Ligar câmera
+                  [3] - Tirar foto''')
+                
+            escolha_opcao = int(input("Deseja qual opção da câmera: "))
+            if escolha_opcao == 1:
+                desligar_camera()
+            elif escolha_opcao == 2:
+                ligar_camera()
+            elif escolha_opcao == 3:
+                tirar_foto()
+            else:
+                print("Opção inválida")
+                
         elif opcao == 6:
             print("  Histórico  ")
+            ver_historico()
+            
         elif opcao == 0:
             print("  Encerra o programa  ")
             break
